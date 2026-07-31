fdcb73a1132a6c9cc3896e7b36a682a8396fd036
anaduphilip <anaduphilip2000@gmail.com>
2026-07-31 14:50:29 +0100

    updated server

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(cors({
     return callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 app.options('*', cors());
